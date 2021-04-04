b121a5a2ed3beaf0f17cd92bcd4baed840039cd3
bebeidon <51832840+bebeidon@users.noreply.github.com>
2021-04-04 23:16:46 +0200

    Update RetroAchievements.cpp

--- es-app/src/RetroAchievements.cpp
+++ es-app/src/RetroAchievements.cpp
@@ -31,6 +31,7 @@ const std::map<PlatformId, unsigned short> cheevosConsoleID
 	{ NINTENDO_ENTERTAINMENT_SYSTEM, 7 },
 	{ TURBOGRAFX_16, 8 },
 	{ TURBOGRAFX_CD, 8 },
+	{ MEGA_CD, 9 },
 	{ SEGA_CD, 9 },
 	{ SEGA_32X, 10 },
 	{ SEGA_MASTER_SYSTEM, 11 },
